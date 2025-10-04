a05ff7d26700888f1cf2994b39fa3a0de92edffd
eboran <emre.boran@aifalabs.com>
2025-10-04 13:38:01 +0300

    feat: add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">my-next-app</h1>
+      <p className="text-gray-700">Hello Next.js App Router 👋</p>
+    </main>
+  );
+}
